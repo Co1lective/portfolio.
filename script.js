0244e0bad5f9793f4c63eabb4977a88dbe226861
Co1lective <160243499+Co1lective@users.noreply.github.com>
2024-04-18 19:58:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ async function sendInfo(event) {
         embeds: [{
             title: 'Feedback Form Submission',
             fields: [
-                { name: 'Hi lol'}
+                { name: 'Hi lol :O'}
             ],
             color: '12618607'
         }],
